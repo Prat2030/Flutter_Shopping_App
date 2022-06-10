99900576cae2c4cf0e4433176678d073cb04ad5d
Prat2030 <pratyushjindal2003@gmail.com>
2022-06-10 14:05:07 +0530

    created product item widget and added items to product overview screen using GridTile

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 
+import '../widgets/product_item.dart';
 import '../models/product.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
@@ -49,14 +50,16 @@ class ProductsOverviewScreen extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
         itemCount: loadedProducts.length,
-        itemBuilder: (BuildContext context, int index) => Container(),
+        itemBuilder: (BuildContext context, int i) => ProductItem(
+            id: loadedProducts[i].id,
+            title: loadedProducts[i].title,
+            imageUrl: loadedProducts[i].imageUrl),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        
       ),
     );
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
-  
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({required this.id, required this.title, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridTile(
+      child: Image.network(imageUrl, fit: BoxFit.cover),
+      footer: GridTileBar(
+        backgroundColor: Colors.black87,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(title),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          color: Colors.white,
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
