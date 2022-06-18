2ff8aaa89a567aa32dd042c3e57dea56ea06b74b
Prat2030 <pratyushjindal2003@gmail.com>
2022-06-18 13:23:08 +0530

    implemented confirmDismiss feature while removing item from the cart using showDialog

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -45,8 +45,8 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
-              Scaffold.of(context).hideCurrentSnackBar();
-              Scaffold.of(context).showSnackBar(SnackBar(
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 content: Text(
                   'Item added to the Cart!',
                   textAlign: TextAlign.center,
