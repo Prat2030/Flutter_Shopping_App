c17774775da77a0353fea896d92249f19988b499
Prat2030 <pratyushjindal2003@gmail.com>
2022-06-18 13:48:05 +0530

    implemented user products screen for all available products with required actions

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  UserProductItem({required this.title, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/edit-product');
+              },
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/edit-product');
+              },
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
